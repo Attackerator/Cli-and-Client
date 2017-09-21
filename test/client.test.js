a8dd250158198a7dc43ccdebc7e4bc67fc11e2a3
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-09-21 11:01:23 -0500

    signin test

--- test/client.test.js
+++ test/client.test.js
@@ -10,13 +10,25 @@ const testBody = {
   ,email: 'test@example.com'
 };
 
-describe('clienty thing', function(){
-  xit('should have a response not an error?', function(){
-    return client.newUser(testBody)
-      .then(res => {
-        debug(res.text);
-        expect(res.text.substring(0, 36)).to.equal('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9');
-      })
-      .catch(err => debug(err));
+describe('client-server interaction', function(){
+  describe('users creation', function(){
+    it('should have return a token in the response', function(){
+      return client.newUser(testBody)
+        .then(res => {
+          debug(res.text);
+          expect(res.text.substring(0, 36)).to.equal('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9');
+        })
+        .catch(err => debug(err));
+    });
+  });
+  describe('users signin', function(){
+    it('should return a token', function(){
+      return client.signIn(testBody.username, testBody.password)
+        .then(res => {
+          debug(res.text);
+          expect(res.text.substring(0, 36)).to.equal('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9');
+        })
+        .catch(err => debug(err));
+    });
   });
 });
